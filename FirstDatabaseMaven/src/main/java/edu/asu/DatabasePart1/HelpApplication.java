f83e919c27f292317f2cd8ad9419755cf3f3c86a
JCa6rera <cabrerajc2004@gmail.com>
2024-10-08 18:54:53 -0700

    Getting JavaFX to run
    Commit is to just make sure that java fx is running fine.

---
+++ FirstDatabaseMaven/src/main/java/edu/asu/DatabasePart1/HelpApplication.java
@@ -0,0 +1,27 @@
+package edu.asu.DatabasePart1;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class HelpApplication extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			System.out.println("testing");
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
