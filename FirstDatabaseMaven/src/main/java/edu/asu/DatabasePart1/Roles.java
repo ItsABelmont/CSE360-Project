a431eedd3fcaea00167b3bf61a096e41c0598b07
Ufuueueuru <theultimatewiseguy@gmail.com>
2024-10-09 12:52:13 -0700

    Created static Roles class

---
+++ FirstDatabaseMaven/src/main/java/edu/asu/DatabasePart1/Roles.java
@@ -0,0 +1,21 @@
+package edu.asu.DatabasePart1;
+
+public class Roles {
+	/**
+	 * Takes an input of the form "admin,instructor,student" and returns a String[]
+	 * @param input
+	 * @return
+	 */
+	public static String[] stringToArray(String input) {
+		return input.split(",");
+	}
+	
+	/**
+	 * Takes an input of a String array and returns a single string with each role separated by commas
+	 * @param input
+	 * @return
+	 */
+	public static String ArrayToString(String[] input) {
+		return String.join(",", input);
+	}
+}
