cbff012c7723761a536ddf5a5004fcd2a17e684c
reemhesham-g <rhhelal@asu.edu>
2024-12-04 20:10:05 -0700

    Article Organization JUnit
    let's see

---
+++ FirstDatabaseMaven/src/test/java/JUnit_TestSuite.java
@@ -0,0 +1,16 @@
+import edu.asu.DatabasePart1.JUnit_ArticleOrganization;
+import org.junit.runner.RunWith;
+
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	JUnit_ArticleOrganization.class
+	
+})
+public class JUnit_TestSuite {
+	
+}
